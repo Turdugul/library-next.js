1266c326411e356b120a22341441433d38de41cd
Turdugul <okonbaevaturdugul@gmail.com>
2025-01-12 20:43:38 +0000

    completed auth: sign-in, sign-up page

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,19 @@
+'use client'
+import React from 'react'
+import { signInSchema } from '../../../lib/validations';
+import AuthForm from '@/components/AuthForm';
+
+
+const page = () => (
+    <AuthForm
+    type='SIGN_IN'
+    schema={signInSchema}
+    defaultValues={{
+         email: '',
+        password: '',
+    }}
+    onSubmit={()=> {}}
+      />
+    )
+
+export default page
